6db615360d8694f5640d57e8a315b7c4b98f0a8c
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-10-05 11:25:55 -0300

    Adicionando aula 9

---
+++ aula9_array/script.js
@@ -0,0 +1,26 @@
+//*isso é um array
+const alunos = ["Ana", "Hugo", "Marta"] 
+const notas = [9, 10, 8]
+
+//acesso ao array
+alunos[2]
+
+
+//*isto é um objeto
+const aluno1 = {
+    'nome':"Ana",
+    "nota":9
+}
+//acesso ao objeto
+aluno1.nome
+
+//isso é um array que possui objetos
+const alunoDS = [
+    {"nome": "Ana", "nota":9},
+    {"nome": "Hugo", "nota":10},
+    {"nome": "Marta", "nota":8}
+]
+alunoDS[0].nota
+
+//? Para acessarmos um objeto utilizamos um . (ponto)
+//? Para acessarmos um array utilizamos um [] colchete e a posição
